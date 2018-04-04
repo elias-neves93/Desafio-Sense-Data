a2aa07600e4b8568ad5a63ea976f5b125dfb3652
Elias Neves <elias-neves93@hotmail.com>
2018-04-04 17:59:44 -0300

    ALterações

--- model.py
+++ model.py
@@ -1,34 +1,37 @@
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy import Column,Integer,String,ForeignKey, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 
-engine = create_engine("sqlite:///banco.db")
-Base = declarative_base()
-Session = sessionmaker()
-Session.configure(bind=engine)
 
-session = Session()
-
-class TODO(Base):
-    __tablename__ = "todo"
-    id = Column(Integer,primary_key=True)
-    task = Column(String)
-    user = Column(String)
-
-if __name__ == "__main__":
-    try:
-        Base.metadata.create_all(engine)
-        #funcionario = Funcionario(id=1,nome="Elias Neves")
-        #session.add(funcionario)
-        #dependente1 = Dependentes(id=1,nome="Joao")
-        #dependente2 = Dependentes(id=2,nome="Maria")
-        #funcionario.dependentes.append(dependente1)
-        #funcionario.dependentes.append(dependente2)
-        #session.add(dependente1)
-        #session.add(dependente2)
-        #session.commit()
-    except Exception as e:
-        print ("Falhou ao criar o banco: {}".format(e))
+#engine = create_engine("sqlite:///banco.db")
+#Base = declarative_base()
+#Session = sessionmaker()
+#Session.configure(bind=engine)
+#
+#session = Session()
+#
+#class Todo(Base):
+#    __tablename__ = "todo"
+#    id = Column(Integer,primary_key=True)
+#    task = Column(String)
+#    user = Column(String)
+#    status = Column(Boolean)
+#
+#if __name__ == "__main__":
+#    try:
+#        Base.metadata.create_all(engine)
+#        #funcionario = Funcionario(id=1,nome="Elias Neves")
+#        #session.add(funcionario)
+#        #dependente1 = Dependentes(id=1,nome="Joao")
+#        #dependente2 = Dependentes(id=2,nome="Maria")
+#        #funcionario.dependentes.append(dependente1)
+#        #funcionario.dependentes.append(dependente2)
+#        #session.add(dependente1)
+#        #session.add(dependente2)
+#        #session.commit()
+#    except Exception as e:
+#        print ("Falhou ao criar o banco: {}".format(e))
+#
--- run.py
+++ run.py
@@ -1,31 +1,97 @@
 # -*- coding: UTF-8 -*-
 
-from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+#from flask import Flask, render_template, flash, request
+#from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+#from Blueprints.get_form_task import get_form_task
+#
+import json
 
+from flask import Flask, request, jsonify
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'banco.sqlite')
 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
 
-@app.route("/")
-def index():
 
-    form = ReusableForm(request.form)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-    print (form.errors)
-    if request.method == 'POST':
-        name=request.form['name']
-        print (name)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(80), unique=True)
+    status = db.Column(db.Boolean(120), unique=True)
 
-        if form.validate():
-            # Save the comment here.
-            flash('Hello ' + name)
-        else:
-            flash('All the form fields are required. ')
+    def __init__(self, username, email):
+        self.task = task
+        self.status = status
 
-    return render_template('index.html', form=form)
 
-if __name__ == "__main__":
+class TaskSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('task', 'status')
+
+
+task_schema = TaskSchema()
+task_schema = TaskSchema(many=True)
+
+# Criar nova task
+@app.route("/task", methods=["POST"])
+def add_task():
+    task = request.json['task']
+    status = request.json['status']
+
+    new_task = Task(task, status)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify(new_task)
+
+
+# Mostrar todas task
+@app.route("/task", methods=["GET"])
+def get_task():
+    all_task = Task.query.all()
+    result = task_schema.dump(all_task)
+    return jsonify(result.data)
+
+
+## endpoint to get user detail by id
+#@app.route("/user/<id>", methods=["GET"])
+#def user_detail(id):
+#    user = User.query.get(id)
+#    return user_schema.jsonify(user)
+
+
+## endpoint to update user
+#@app.route("/user/<id>", methods=["PUT"])
+#def user_update(id):
+#    user = User.query.get(id)
+#    username = request.json['username']
+#    email = request.json['email']
+#
+#    user.email = email
+#    user.username = username
+#
+#    db.session.commit()
+#    return user_schema.jsonify(user)
+
+
+#deletar task
+@app.route("/task/<id>", methods=["DELETE"])
+def task_delete(id):
+    task = Task.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return task_schema.jsonify(task)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
